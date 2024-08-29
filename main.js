5f5575d87412f5fe06bb28d7a134bf08c254c043
Ghilman Faza <72027610+gffaza@users.noreply.github.com>
2024-08-29 16:08:46 +0700

    update express

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+//minta data jokes dair sistem luar
+
+app.get('/', async(req, res) => {
+    try{
+        const response = await axios.get('https://v2.jokeapi.dev/joke/Dark?type=single');
+        const joke = response.data.joke;
+        res.json({joke})
+    }catch(error){
+        res.status(500).json({error : 'Failed get data'});
+    }
 })
 
 app.listen(port, () => {
