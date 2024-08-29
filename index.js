e947879fbf1536524c3cff1a7745b87988ddf218
Ghilman Faza <72027610+gffaza@users.noreply.github.com>
2024-08-29 14:06:29 +0700

    new file

---
+++ index.js
@@ -0,0 +1,19 @@
+//siapkan berkas rumah
+const { createServer } = require('node:http');
+
+//tentuin dimana alamat rumahnya
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//perintahkan untuk buatkan rumah
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+
+//rumah bisa didatangi
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
