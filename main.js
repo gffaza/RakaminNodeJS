c8d28000556ed0dc294b1e1ac52cea5a9ddfb7c7
Ghilman Faza <72027610+gffaza@users.noreply.github.com>
2024-08-29 17:07:15 +0700

    done node express

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const express = require('express')
-const app = express()
-const port = 3000
 const axios = require('axios');
+const app = express();
+const port = 5501;
 
+//baca file static
+app.use(express.static('public'));
+//confert response to json
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname +  '/views/index.html' );
+})
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
 // })
+
+
 //minta data jokes dair sistem luar
 
-app.get('/', async(req, res) => {
+app.get('/joke', async(req, res) => {
     try{
         const response = await axios.get('https://v2.jokeapi.dev/joke/Dark?type=single');
         const joke = response.data.joke;
